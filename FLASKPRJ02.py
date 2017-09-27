d65c0812001f62e708bd6fd35202c5b9788b288d
dev@silvatech.org <Password&2017>
2017-09-27 15:43:07 -0600

    Returning Data back to front end script preparing back end first.

--- FLASKPRJ02.py
+++ FLASKPRJ02.py
@@ -20,8 +20,28 @@ def home():
 @app.route('/add', methods=['POST'])
 def add():
     try:
-        data = request.form.get("userinput")
+        data = request.form.get('userinput')
         DB.add_input(data)
+    except Exception as e:
+        print(e)
+    return home()
+
+
+@app.route('/submitbusiness', methods=['POST'])
+def submitbusiness():
+    try:
+        bizname = request.form.get('bizname')
+        bizaddr = request.form.get('bizaddr')
+        usrtel = request.form.get('usrtel')
+        email = request.form.get('email')
+        homepage = request.form.get('homepage')
+        category = request.form.get('category')
+        date = request.form.get('date')
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+        description = request.form.get('description')
+        DB.add_business(bizname, bizaddr, usrtel, email, homepage, category, date, latitude, longitude, description)
+        return home()
     except Exception as e:
         print(e)
     return home()
@@ -37,33 +57,7 @@ def clear():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
-# @app.route('/')
-# def home():
-#     try:
-#         data = DB.get_all_inputs()
-#     except Exception as e:
-#         print(e)
-#         data = None
-#     return render_template("home.html", data=data)
-#
-#
-# @app.route('/add', methods=['POST'])
-# def add():
-#     try:
-#         data = request.form.get("userinput")
-#         DB.add_input(data)
-#     except Exception as e:
-#         print(e)
-#     return home()
-#
-#
-# @app.route('/clear')
-# def clear():
-#     try:
-#         DB.clear_all()
-#     except Exception as e:
-#         print(e)
-#     return home()
+
 
--- dbhelper.py
+++ dbhelper.py
@@ -13,7 +13,7 @@ class DBHelper:
     def get_all_inputs(self):
         connection = self.connect()
         try:
-            query = "SELECT description FROM business_list;"
+            query = "SELECT * FROM business_list;"
             with connection.cursor() as cursor:
                 cursor.execute(query)
             return cursor.fetchall()
@@ -39,7 +39,48 @@ class DBHelper:
                 connection.commit()
         finally:
             connection.close()
-##Test Input here
+
+    def add_business(self,bizname,bizaddr,usrtel,email,homepage,category,date,latitude,longitude,description):
+        connection = self.connect()
+        try:
+            query = "INSERT INTO business_list (name_of_business,address,phone" \
+                    ",email,url,category,in_date,latitude,longitude,description) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"
+            with connection.cursor() as cursor:
+                cursor.execute(query,(bizname, bizaddr, usrtel, email, homepage, category, date, latitude, longitude,
+                                      description))
+                connection.commit()
+        except Exception as e:
+            print(e)
+        finally:
+            connection.close()
+
+    def get_all_business(self):
+        connection = self.connect()
+        try:
+            query = "SELECT name_of_business,address,phone,email,url,category,in_date,latitude,longitude,description FROM business_list"
+            with connection.cursor() as cursor:
+                cursor.execute(query)
+            named_businesses = []
+            for business in cursor:
+                name_business = {
+                    'bizname': business[1],
+                    'bizaddr': business[2],
+                    'usrtel': business[3],
+                    'email': business[4],
+                    'homepage': business[5],
+                    'date': business[6],
+                    'category': business[7],
+                    'latitude': business[8],
+                    'longitude': business[9],
+                    'description': business[10]
+                }
+                named_businesses.append(name_business)
+            return named_businesses
+        finally:
+            connection.close()
+
+
+    #Bulding out just text submission to database table
     def get_all_inputs1(self):
         connection = self.connect()
         try:

0cded7fb65207a942e80ddf62c3f5612e114ba87
dev@silvatech.org <Password&2017>
2017-10-10 08:00:49 -0600

    Added new get all businesses function and passed it into main py file to then pass into page.

--- FLASKPRJ02.py
+++ FLASKPRJ02.py
@@ -2,13 +2,19 @@ from dbhelper import DBHelper
 from flask import Flask
 from flask import render_template
 from flask import request
+import html
+import datetime
+import dateparser
 import json
+import string
 app = Flask(__name__)
 DB = DBHelper()
+categories = ['retail','restuarant','bar','fashion-beauty','hardware','autoparts','banks','technology','telecom','pharmacy']
 
 
 @app.route('/')
-def home():
+def home(error_message=None):
+    #global businesses
     try:
         data = DB.get_all_inputs()
         businesses = DB.get_all_business()
@@ -16,7 +22,7 @@ def home():
     except Exception as e:
         print(e)
         data = None
-    return render_template("home.html", data=data, businesses=businesses)
+    return render_template("home.html", data=data, businesses=businesses, categories=categories,error_message=error_message)
 
 
 @app.route('/add', methods=['POST'])
@@ -38,10 +44,15 @@ def submitbusiness():
         email = request.form.get('email')
         homepage = request.form.get('homepage')
         category = request.form.get('category')
-        date = request.form.get('date')
+        if category not in categories:
+            return home()
+        date=format_date(request.form.get('date'))
+        if not date:
+            return home("Invalid date please use correct format")
         latitude = float(request.form.get("latitude"))
         longitude = float(request.form.get("longitude"))
-        description = request.form.get('description')
+        description = html.escape(request.form.get('description'))
+        #description = sanitized_string(request.form.get('description'))
         DB.add_business(bizname, bizaddr, usrtel, email, homepage, category, date, latitude, longitude, description)
         return home()
     except Exception as e:
@@ -56,6 +67,19 @@ def clear():
     except Exception as e:
         print(e)
     return home()
+
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date,"%Y-%m-%d")
+    except TypeError:
+        return None
+
+
+def sanitized_string(userinput):
+    whitelist = string.ascii_letters + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
 
 
 if __name__ == '__main__':
--- dbhelper.py
+++ dbhelper.py
@@ -1,6 +1,6 @@
 import pymysql
 import dbconfig
-
+import datetime
 
 class DBHelper:
 
@@ -57,22 +57,25 @@ class DBHelper:
     def get_all_business(self):
         connection = self.connect()
         try:
-            query = "SELECT name_of_business,address,phone,email,url,category,in_date,latitude,longitude,description FROM business_list"
+            query = "SELECT name_of_business,address,phone,email,url,in_date,latitude,longitude,category,description FROM business_list"
+            #query = "SELECT latitude,longitude,in_date,category,description FROM business_list"
             with connection.cursor() as cursor:
                 cursor.execute(query)
             named_businesses = []
             for business in cursor:
                 name_business = {
-                    'bizname': business[1],
-                    'bizaddr': business[2],
-                    'usrtel': business[3],
-                    'email': business[4],
-                    'homepage': business[5],
-                    'date': business[6],
-                    'category': business[7],
-                    'latitude': business[8],
-                    'longitude': business[9],
-                    'description': business[10]
+
+                    'bizname': business[0],
+                    'bizaddr': business[1],
+                    'usrtel': business[2],
+                    'email': business[3],
+                    'homepage': business[4],
+                    'date': datetime.datetime.strftime(business[5], '%Y-%m-%d'),
+                    'latitude': business[6],
+                    'longitude': business[7],
+                    'category': business[8],
+                    'description': business[9],
+
                 }
                 named_businesses.append(name_business)
             return named_businesses
